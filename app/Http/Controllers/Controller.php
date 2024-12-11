a7f5415bf496e7d0bd3e7487ad58643caacea51b
CobbyElsonfx <andohfrancis9187@gmail.com>
2024-12-11 13:28:12 +0000

    added documentation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-
+/**
+ * @OA\Info(
+ *     title="Todo API",
+ *     version="1.0",
+ *     description="This is the API documentation for a Todo app. It allows you to perform Basic CRUD operations on todos with filtering, sorting, and searching on all the todos",
+ *     @OA\Contact(
+ *         email="andohfrancis9187@gmail.com"
+ *     )
+ * )
+ */
 abstract class Controller
 {
     //
